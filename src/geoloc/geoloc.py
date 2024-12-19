52a2ff56faf8e42f11bc758291862444523571fc
Dmitriy Chernoshey <dmitriych@yahoo.com>
2024-12-19 11:19:07 -0600

    Update geoloc.py

--- src/geoloc/geoloc.py
+++ src/geoloc/geoloc.py
@@ -1,9 +1,8 @@
 import argparse
 import requests
 import json
-import secrets
 
-# NOTE: normally, the secrets.API_KEY would not be included in the source code, especially of a public repository. 
+# NOTE: normally, API_KEY would not be included in the source code, especially of a public repository. 
 # The proper way to do is to store it in a secure place, such as a password manager or cloud secrets storage and
 # extract it programmatically. For example, CircleCI can be configured with a secret key env variable.
 
